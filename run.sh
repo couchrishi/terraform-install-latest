6b9db4ffad493aab1039d7f763b770361db5b454
defacer <saibalaji@outlook.com>
2018-11-21 14:37:17 +0530

    changes
    changes

--- run.sh
+++ run.sh
@@ -1,23 +1,22 @@
-#!/bin/bash
-function terraform-install() {
-[[ -f ${HOME}/bin/terraform ]] && echo "`${HOME}/bin/terraform version` already installed at ${HOME}/bin/terraform" && return 0
-LATEST_URL=$(curl -sL https://releases.hashicorp.com/terraform/index.json | jq -r '.versions[].builds[].url' | sort -t. -k 1,1n -k 2,2n -k 3,3n -k 4,4n | egrep -v 'rc|beta' | egrep 'linux.*amd64' |tail -1)
-curl ${LATEST_URL} > /tmp/terraform.zip
-mkdir -p ${HOME}/bin
-(cd ${HOME}/bin && unzip /tmp/terraform.zip)
-if [[ -z $(grep 'export PATH=${HOME}/bin:${PATH}' ~/.bashrc) ]]; then
-  echo 'export PATH=${HOME}/bin:${PATH}' >> ~/.bashrc
-fi
+#!/bin/bash -x
 
-echo "Installed: `${HOME}/bin/terraform version`"
-
-cat - << EOF 
-
-Run the following to reload your PATH with terraform:
-source ~/.bashrc
-rm "terraform.zip"
-terraform --v
-EOF
+trim() {
+  # shellcheck disable=SC2001
+  echo "$1" | sed -e "s/\\\n\$//"
 }
 
-terraform-install
+cli_args=
+
+if [ -n "${WERCKER_TERRAFORM_VAR_FILE}" ]; then
+  cli_args="$cli_args -var-file=${WERCKER_TERRAFORM_VAR_FILE}"
+fi
+
+terraform_cli="${WERCKER_STEP_ROOT}/terraform"
+$terraform_cli --version
+
+$terraform_cli init
+
+echo "terraform ${WERCKER_TERRAFORM_COMMAND} $cli_args"
+if ! eval "$terraform_cli ${WERCKER_TERRAFORM_COMMAND} $cli_args"; then
+  fail "Invalid command option"
+fi
