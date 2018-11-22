233b5fb49d73246690e68cd3efc76065d01b7761
defacer <saibalaji@outlook.com>
2018-11-22 19:41:02 +0530

    changes
    changes

--- run.sh
+++ run.sh
@@ -7,13 +7,6 @@ trim() {
 
 terraform_cli="${WERCKER_STEP_ROOT}/terraform"
 
-if [ -n "${WERCKER_TERRAFORM_INSTALL_LATEST_VAR_FILE}" ]; then
-  cd $terraform_cli
-  echo ${WERCKER_TERRAFORM_INSTALL_LATEST_VAR_FILE} > temp.tfvars
-  awk '{gsub(/\\n/,"\n")}1' temp.tfvars > terraform.tfvars
-  rm temp.tfvars
-fi
-
 $terraform_cli --version
 
 $terraform_cli init
