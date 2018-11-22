491fdab8bbc15b02e95b999697d23c3adbc31321
defacer <saibalaji@outlook.com>
2018-11-23 03:39:27 +0530

    changes
    changes

--- run.sh
+++ run.sh
@@ -12,7 +12,7 @@ $terraform_cli --version
 $terraform_cli init
 
 cat ${WERCKER_TERRAFORM_INSTALL_LATEST_VAR_FILE}
-echo "terraform ${WERCKER_TERRAFORM_INSTALL_LATEST_COMMAND} -var-file="${WERCKER_TERRAFORM_INSTALL_LATEST_VAR_FILE}"
+echo "terraform ${WERCKER_TERRAFORM_INSTALL_LATEST_COMMAND} -var-file="${WERCKER_TERRAFORM_INSTALL_LATEST_VAR_FILE}"""
 if ! eval "$terraform_cli ${WERCKER_TERRAFORM_INSTALL_LATEST_COMMAND -var-file="${WERCKER_TERRAFORM_INSTALL_LATEST_VAR_FILE}"""; then
   fail "Invalid command option"
 fi
